eed0330895eb5a0bdc9eac3d9b8f4ef995d71041
01dox <167974560+01dox@users.noreply.github.com>
2025-03-25 18:29:27 +0300

    uploaded
    Merging Arrays is what is in the file

--- main.cpp
+++ main.cpp
@@ -1,46 +1,39 @@
 #include <iostream>
-#include<iomanip>
-using namespace std;
+#include <vector>
+#include <string>
 
+int main() {
+    int N, X;
+    std::vector<std::string> animals;
+    std::string animal;
 
-double CalculateSubjectAverage(double Assignment,double CourseWork,double MidTerm,double EndTerm)
-{
-    return (Assignment + CourseWork + MidTerm + EndTerm)/4.0;
-}
+    // Get number of domestic animals
+    std::cout << "Enter the number of domestic animals: ";
+    std::cin >> N;
+    std::cin.ignore();  // Ignore the newline character
 
-int main()
-{
-
-    double Physics[4],Chemistry[4],Math[4];
-    string Categories[] = {"Assignment", "CourseWork", "MidTerm", "EndTerm"};
-
-    cout <<"Enter marks for Physics:" <<endl;
-    for(int i = 0; i<4; i++){
-        cout << Categories[i]<<": ";
-        cin >>Physics[i];
-    }
-    cout <<"Enter marks for Chemistry:" <<endl;
-    for(int i = 0; i<4; i++){
-        cout << Categories[i]<<": ";
-        cin >>Chemistry[i];
-         }
-         cout <<"Enter marks for Math:" <<endl;
-    for(int i = 0; i<4; i++){
-        cout << Categories[i]<<": ";
-        cin >>Math[i];
+    std::cout << "Enter the names of " << N << " domestic animals:\n";
+    for (int i = 0; i < N; ++i) {
+        std::getline(std::cin, animal);
+        animals.push_back(animal);
     }
 
-    double PhysicsAverage = CalculateSubjectAverage(Physics[0],Physics[1],Physics[2],Physics[3]);
-    double ChemistryAverage = CalculateSubjectAverage(Chemistry[0],Chemistry[1],Chemistry[2],Chemistry[3]);
-    double MathAverage = CalculateSubjectAverage(Math[0],Math[1],Math[2],Math[3]);
-    double OverallAverage = (PhysicsAverage + ChemistryAverage + MathAverage)/3.0;
+    // Get number of wild animals
+    std::cout << "Enter the number of wild animals: ";
+    std::cin >> X;
+    std::cin.ignore();  // Ignore the newline character
 
+    std::cout << "Enter the names of " << X << " wild animals:\n";
+    for (int i = 0; i < X; ++i) {
+        std::getline(std::cin, animal);
+        animals.push_back(animal);
+    }
 
+    // Print all animals
+    std::cout << "\nList of all animals (" << N + X << "):\n";
+    for (const auto& a : animals) {
+        std::cout << a << "\n";
+    }
 
-    cout <<"\nAverage marks: "<<endl;
-    cout <<"Physics: "<<PhysicsAverage<<endl;
-    cout <<"Chemistry: "<<ChemistryAverage<<endl;
-    cout <<"Math: "<<MathAverage<<endl;
-    cout <<"Overall Average: "<<OverallAverage<<endl;
-       return 0;
+    return 0;
 }
